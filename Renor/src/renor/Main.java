a0b686221c6fa6fc3fca6a17ce79a196a2768807
Jared Diehl <jareddiehl96@gmail.com>
2017-09-10 20:09:36 -0500

    Session added

--- Renor/src/renor/Main.java
+++ Renor/src/renor/Main.java
@@ -2,11 +2,13 @@ package renor;
 
 import java.net.Proxy;
 
+import renor.util.Session;
+
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("java.net.preferIPv4Stack", "true");
 
-		Renor renor = new Renor(null, 854, 480, false, null, Proxy.NO_PROXY, "Indev");
+		Renor renor = new Renor(new Session("Voidx", "69"), 854, 480, false, null, Proxy.NO_PROXY, "Indev");
 
 		Thread.currentThread().setName("Renor main thread");
 		renor.run();
